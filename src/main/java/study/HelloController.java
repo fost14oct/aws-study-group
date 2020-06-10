71538cc65355ca645dd7edd813308326b74b9a96
fost14oct <fausto.arre90@gmail.com>
2020-06-10 18:18:13 -0500

    First commit

---
+++ src/main/java/study/HelloController.java
@@ -0,0 +1,16 @@
+package study;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloController {
+
+    @GetMapping("/test")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello";
+    }
+}
