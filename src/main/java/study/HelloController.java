dbb366113a86e54f3f1b04caf12a1a083372c162
fost14oct <fausto.arre90@gmail.com>
2020-06-11 11:00:25 -0500

    hot fix

--- src/main/java/study/HelloController.java
+++ src/main/java/study/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
 
     @GetMapping("/test")
     public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "Hello";
+        return "HOT FIX";
     }
 }
